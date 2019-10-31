02e836e098bbfb3fbae1c1e537c53b6f127c95b3
Victor Olet <victorolet96@outlook.com>
2019-11-01 03:50:57 +0800

    Setting blog page to home

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -15,26 +15,32 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.urls import path,include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/',user_views.register,name="register"),
-    path('profile/',user_views.profile,name="profile"),
-    path('login/',auth_views.LoginView.as_view(template_name="users/login.html"),name="login"), #These log in views already pre-exist in Django
-    path('logout/',auth_views.LogoutView.as_view(template_name="users/logout.html"),name="logout"),
+    path('register/', user_views.register, name="register"),
+    path('profile/', user_views.profile, name="profile"),
+    path('login/', auth_views.LoginView.as_view(template_name="users/login.html"),
+         name="login"),  # These log in views already pre-exist in Django
+    path('logout/', auth_views.LogoutView.as_view(template_name="users/logout.html"), name="logout"),
     # The password reset view also exists in django
-    path('password-reset/',auth_views.PasswordResetView.as_view(template_name="users/password_reset.html"),name="password_reset"),
-    path('password-reset/done/',auth_views.PasswordResetDoneView.as_view(template_name="users/password_reset_done.html"),name="password_reset_done"),
-    path('password-reset-confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name="users/password_reset_confirm.html"),name="password_reset_confirm"),
-    path('password-reset-complete/',auth_views.PasswordResetCompleteView.as_view(template_name="users/password_reset_complete.html"),name="password_reset_complete"),
-    path('blog/', include("blog.urls")),
+    path('password-reset/', auth_views.PasswordResetView.as_view(
+        template_name="users/password_reset.html"), name="password_reset"),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name="users/password_reset_done.html"), name="password_reset_done"),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name="users/password_reset_confirm.html"), name="password_reset_confirm"),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
+        template_name="users/password_reset_complete.html"), name="password_reset_complete"),
+    path('', include("blog.urls")),
 ]
 
-#Ensures that this runs only in dev(debug) mode.
-#Media root is differen when in production mode.
+# Ensures that this runs only in dev(debug) mode.
+# Media root is differen when in production mode.
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
