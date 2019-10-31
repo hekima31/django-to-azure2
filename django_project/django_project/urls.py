3939ff082057183dd207969aff853bb47f50d107
Victor Olet <victorolet96@outlook.com>
2019-11-01 03:53:48 +0800

    Wildcard

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -36,7 +36,7 @@ urlpatterns = [
         template_name="users/password_reset_confirm.html"), name="password_reset_confirm"),
     path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
         template_name="users/password_reset_complete.html"), name="password_reset_complete"),
-    path('', include("blog.urls")),
+    path('blog/', include("blog.urls")),
 ]
 
 # Ensures that this runs only in dev(debug) mode.
