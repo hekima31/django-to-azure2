9529ec6dc47f98ecdc7f02b5fa66ae92dc38012e
Victor Olet <victorolet96@outlook.com>
2019-12-24 02:13:04 +0800

    Add Procfile

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["iamlearn.herokuapp.com"]
+ALLOWED_HOSTS = ["iamlearn.herokuapp.com", "localhost"]
 
 
 # Application definition
