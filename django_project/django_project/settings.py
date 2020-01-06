7b0f3c1a4bb4623f36032239acc281233b3728dc
Victor Olet <victorolet96@outlook.com>
2020-01-06 11:04:07 +0800

    Updated Settings.py file and env

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG_VALUE")
 
 ALLOWED_HOSTS = ["iamlearn.herokuapp.com", "localhost", "127.0.0.1"]
 
