a984041da0890b035f021301c97df45f5ea54bfa
Victor Olet <victorolet96@outlook.com>
2020-06-27 02:24:13 +0800

    Git Changes

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/mnt/c/Users/victo/OneDrive - Curtin University of Technology Australia/Learning/Python/Corey Schafer - Django Framework/venv/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
